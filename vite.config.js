324a3468077445841d69c385e98b26758595863e
DylMon <dylanmonge2002@gmail.com>
2025-08-15 21:31:39 -0700

    domain URL config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-site/'   // repo name; required unless using username.github.io or a custom domain
+  base: '/'   // repo name; required unless using username.github.io or a custom domain
 })
